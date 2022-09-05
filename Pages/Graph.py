9837c20b907db418b31ad349a5363e285d9540c2
Talol-efron <e205713@ie.u-ryukyu.ac.jp>
2022-09-05 18:02:28 +0900

    Graphing in progress

--- Pages/Graph.py
+++ Pages/Graph.py
@@ -3,4 +3,37 @@ import pandas as pd
 import matplotlib as plt
 
 df = pd.read_csv("~/Practice/python/musle/record.csv")
-#Graph仰臥がむずい.matplotlibが有望
+#Graph描画がむずい.matplotlibが有望
+#matplotlibを使うにはst.pylot()が必要
+df_columns = df.columns
+st.markdown("#### 入力データ")
+st.dataframe(df.style.highlight_max(axis=0))
+#matplotlibで可視化。X軸,Y軸を選択できる
+st.markdown("### 可視化 単変量")
+#データフレームのカラムを選択オプションに設定する
+x = st.selectbox("X軸", df_columns)
+y = st.selectbox("Y軸", df_columns)
+#選択した変数を用いてmtplotlibで可視化
+fig = plt.figure(figsize= (12,8))
+plt.scatter(df[x],df[y])
+plt.xlabel(x,fontsize=18)
+plt.ylabel(y,fontsize=18)
+st.pyplot(fig)
+
+"""
+df_columns = df.columns
+#データフレームを表示
+st.markdown("### 入力データ")
+st.dataframe(df.style.highlight_max(axis=0))
+#matplotlibで可視化。X軸,Y軸を選択できる
+st.markdown("### 可視化 単変量")
+#データフレームのカラムを選択オプションに設定する
+x = st.selectbox("X軸", df_columns)
+y = st.selectbox("Y軸", df_columns)
+#選択した変数を用いてmtplotlibで可視化
+fig = plt.figure(figsize= (12,8))
+plt.scatter(df[x],df[y])
+plt.xlabel(x,fontsize=18)
+plt.ylabel(y,fontsize=18)
+st.pyplot(fig)
+"""
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 How to run
 streamlit run app.py
 
-use streamlit
+Reference page
+Basic
+https://data-analytics.fun/2022/01/29/understanding-streamlit-1/
+Graph
+https://zenn.dev/littledarwin/articles/581c15b3a061cb
+
+Memo
+st.markdown() -> Can be sized using #.
+#-> ## -> ### -> ...
